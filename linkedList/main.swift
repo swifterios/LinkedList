4cac71ddd0415c9b915c0925a4aef198751ec995
Vladislav <lordroga2@gmail.com>
2021-09-01 22:23:24 +0300

    Initial Commit

---
+++ linkedList/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  linkedList
+//
+//  Created by Владислав on 01.09.2021.
+//
+
+import Foundation
+
+
+class LinkedList<T> {
+    var head: Node<T>?
+    var first: Node<T>? {
+        return head
+    }
+    var last: Node<T>? {
+        guard var node = head else { return nil}
+        while let next = node.next {
+            node = next
+        }
+    }
+}
+
+class Node<T> {
+    var value: T
+    var next: Node?
+    var previos: Node?
+    
+    init(value: T) {
+        self.value = value
+    }
+}
